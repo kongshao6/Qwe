9325e8953547919fa39ace32bb3ed7ec8dac5518
kongshao6 <2077812452@qq.com>
2025-11-18 14:36:33 +0800

    Add Kshub.lua with UI and functionality sections

---
+++ Kshub.lua
@@ -0,0 +1,24 @@
+local Start = tick() --启动用
+local ui = loadstring(game:HttpGet("https://pastebin.com/raw/3vQdAJn"))() -- 显示脚本的UI库
+
+local win = ui:new("空少hub")
+local UITab1 = win:Tab("公告","7734068321") -- 左侧边栏分类
+local UITab2 = win:Tab("通用","7734068321") -- 左侧边栏分类
+local UITab3 = win:Tab("race cliker有汉化","7734068321") -- 左侧边栏分类
+
+local about = UITab1:section("公告",true) -- 分类内功能分类
+about:Label("奥你啊")
+about:Label("作者qq2077812452")
+about:Label("私人脚本")
+about:Label("倒卖死全家")
+
+local about = UITab2:section("功能",true) -- 分类内功能分类
+about:Slider --（滑轴输入类）
+end)
+
+about:Toggle --（开关类）
+end)
+
+about:Button("启动别的功能或脚本",function() --（单点类）
+ loadstring(game:HttpGet("脚本链接"))()
+end)
